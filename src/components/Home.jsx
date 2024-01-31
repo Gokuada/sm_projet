f299b9c0bbb7203a136535491593d04d7565a204
Goku <goku@MacBook-Air-de-Goku.local>
2024-01-31 17:15:20 +0100

    fix assets home center

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
     <div className="bg-neutralSilver" id="home">
       <div className="ax-w-screen-x p-4 pb-0 about-container">
         <div className="my-28 md:my-8 py-12 pt-40 flex flex-col w-full mx-auto md:flex-row-reverse items-center justify-between gap-">
-          <div className="mb-6 md:mb-0 md:w-1/2">
-            <img src={bike} alt="" className="max-w-full h-auto" />
+          <div className="mb-6 md:mb-1 md:w-5/3">
+            <img src={bike} alt="" className="max-w-full h-auto mb-4 md:mb-0 md:mr-8" />
           </div>
           <div className="md:w-1/2 flex flex-col">
             <h1 className="text-4xl md:text-6xl mb-3 md:mb-5 font-semibold text-white md:w-2/3 leading-snug">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,45 +4,46 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import sm from "../assets/sm.png";
 
 const Popup = ({ isOpen, onClose }) => {
-    const handleOverlayClick = (e) => {
-      if (e.target.classList.contains("bg-opacity-50")) {
-        onClose();
-      }
-    };
-    return (
+  const handleOverlayClick = (e) => {
+    if (e.target.classList.contains("bg-opacity-50")) {
+      onClose();
+    }
+  };
+
+  return (
     <div
-      className={`fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center ${
+      className={`fixed inset-0 bg-black bg-opacity-50 backdrop-filter-blur z-50 flex items-center justify-center ${
         isOpen ? "" : "hidden"
       }`}
       onClick={handleOverlayClick}
     >
       <div className="bg-white p-8 rounded-lg">
-        <section class="bg-white dark:bg-gray-900">
-  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactez-nous</h2>
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Rencontrez-vous des problèmes techniques ? Souhaitez-vous nous faire part de vos commentaires sur une fonctionnalité ? Besoin d'informations sur notre plan Business ? N'hésitez pas à nous en informer.</p>
-      <form action="#" class="space-y-8">
-          <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre mail</label>
-              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+        <section className="bg-white dark:bg-gray-900">
+          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactez-nous</h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Rencontrez-vous des problèmes techniques ? Souhaitez-vous nous faire part de vos commentaires sur une fonctionnalité ? Besoin d'informations sur notre plan Business ? N'hésitez pas à nous en informer.</p>
+            <form action="#" className="space-y-8">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre mail</label>
+                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+              </div>
+              <div>
+                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sujet</label>
+                <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Indiquez-nous comment nous pouvons vous aider." required />
+              </div>
+              <div className="sm:col-span-2">
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Votre message</label>
+                <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Laisse un commentaire..."></textarea>
+              </div>
+              <button
+                type="submit"
+                className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Envoyer le message
+              </button>
+            </form>
           </div>
-          <div>
-              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Sujet</label>
-              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Indiquez-nous comment nous pouvons vous aider." required />
-          </div>
-          <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Votre message</label>
-              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Laisse un commentaire..."></textarea>
-          </div>
-          <button
-  type="submit"
-  className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
->
-  Envoyer le message
-</button>
-      </form>
-            </div>
-            </section>
+        </section>
       </div>
     </div>
   );
@@ -73,7 +74,7 @@ export default function Example() {
     <>
       <Disclosure
         as="nav"
-        className="border-b-2 border-gray bg-gray-800 fixed w-full z-10"
+        className="border-b-2 border-gray backdrop-filter-blur bg-gray-800 fixed w-full z-10"
       >
         {({ open }) => (
           <>

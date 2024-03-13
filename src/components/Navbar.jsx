92f327d1c02f747ee11cb28649ec59b4df3a73de
Goku <goku@MacBook-Air-de-Goku.local>
2024-03-13 16:06:37 +0100

    reduction de popup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,12 +17,12 @@ const Popup = ({ isOpen, onClose }) => {
       }`}
       onClick={handleOverlayClick}
     >
-      <div className="bg-white p-8 rounded-lg">
+      <div className="bg-white p-8 rounded-lg max-w-md w-full mx-auto">
         <section className="bg-white dark:bg-gray-900">
-          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactez-nous</h2>
-            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Rencontrez-vous des problèmes techniques ? Souhaitez-vous nous faire part de vos commentaires sur une fonctionnalité ? Besoin d'informations sur notre plan Business ? N'hésitez pas à nous en informer.</p>
-            <form action="#" className="space-y-8">
+          <div className="py-4 lg:py-8 px-4">
+            <h2 className="mb-4 text-2xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contactez-nous</h2>
+            <p className="mb-4 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-base">Rencontrez-vous des problèmes techniques ? Souhaitez-vous nous faire part de vos commentaires sur une fonctionnalité ? Besoin d'informations sur notre plan Business ? N'hésitez pas à nous en informer.</p>
+            <form action="#" className="space-y-6">
               <div>
                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Votre mail</label>
                 <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
@@ -33,7 +33,7 @@ const Popup = ({ isOpen, onClose }) => {
               </div>
               <div className="sm:col-span-2">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Votre message</label>
-                <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Laisse un commentaire..."></textarea>
+                <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Laisse un commentaire..."></textarea>
               </div>
               <button
                 type="submit"
